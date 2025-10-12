986269ef286629050718f5e542b999bc5db6c31b
xStefanut21 <dragutstefan2018@gmail.com>
2025-10-12 17:43:29 +0300

    solving more errors of build v2.3.1

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   const next = requestUrl.searchParams.get('next') || '/';
 
   if (code) {
-    const supabase = createClient();
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(new URL(next, request.url));
